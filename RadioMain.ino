6b50e3416ae51dcb3bb649cae9df673d8d04e40f
james <pointyfishdude@gmail.com>
2025-05-10 14:13:51 -0400

    Added old code RadioIdeas2 from May 1

--- RadioMain.ino
+++ RadioMain.ino
@@ -1,73 +1,85 @@
 #include <Wire.h>
+#include <RotaryEncoder.h>
 #include <hd44780.h>
 #include <hd44780ioClass/hd44780_I2Cexp.h>
 
-#define PIN_CLK 3
-#define PIN_DT 4
+#define PIN_ROTARY_A A2
+#define PIN_ROTARY_B A3
+#define PIN_ROTARY_BUTTON 12
 
-enum FrequencyType {
+enum FrequencyBand {
   FM, AM
 };
 
+RotaryEncoder encoder(PIN_ROTARY_A, PIN_ROTARY_B, RotaryEncoder::LatchMode::FOUR3);
 hd44780_I2Cexp lcd;
 
-FrequencyType type = AM;
+FrequencyBand band = FM;
 int frequency = 977;
-// int amFrequency = 800;
-int lastFrequency;
-
-volatile int lastClkState = LOW;
-volatile int count;
-int lastCount;
+int alternativeFrequency = 800;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(PIN_CLK, INPUT);
-  pinMode(PIN_DT, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(PIN_CLK), pollRotaryEncoder, CHANGE);
+  pinMode(PIN_ROTARY_BUTTON, INPUT_PULLUP);
 
   int status = lcd.begin(16, 2);
   if (status) {
     hd44780::fatalError(status);
   }
+
+  PrintStationData();
 }
 
 void loop() {
-  delay(100);
+  encoder.tick();
+  if (IsDown(PIN_ROTARY_BUTTON)) {
+    if (band == FM) band = AM;
+    else if (band == AM) band = FM;
 
-  UpdateCurrentFrequency();
+    int temporaryFrequency = frequency;
+    frequency = alternativeFrequency;
+    alternativeFrequency = temporaryFrequency;
 
-  if (frequency != lastFrequency) {
-    lastFrequency = frequency;
-
+    lcd.clear();
     PrintStationData();
   }
 
-  Serial.print("Count: ");
-  Serial.println(count);
+  bool updated = UpdateCurrentFrequency();
+
+  if (updated) {
+    PrintStationData();
+    Tune();
+  }
 }
 
-void UpdateCurrentFrequency() {
-  int currentCount = count;
+bool UpdateCurrentFrequency() {
+  int delta = (int) encoder.getDirection();
 
-  if (currentCount != lastCount) {
-    int delta = currentCount - lastCount;
-
-    if (type == FM) {
-      frequency += delta * 2;
-    } else if (type == AM) {
-      frequency += delta * 10;
-    }
-
-    lastCount = currentCount;
+  if (delta == 0) {
+    return false;
   }
+
+  if (band == FM) {
+
+    frequency += delta * 2;
+    if (frequency < 875) frequency = 1079;
+    else if (frequency > 1079) frequency = 875;
+
+  } else if (band == AM) {
+
+    frequency += delta * 10;
+    if (frequency < 530) frequency = 1710;
+    else if (frequency > 1710) frequency = 530;
+
+  }
+
+  return true;
 }
 
 void PrintStationData() {
   String freq = String(frequency);
-  if (type == FM) {
+  if (band == FM) {
     int length = freq.length();
     freq = freq.substring(0, length-1) + '.' + freq.substring(length-1, length);
   }
@@ -75,13 +87,13 @@ void PrintStationData() {
   String modulation;
   String unit;
   int cursorPosition;
-  GetFrequencyTypeInfo(type, modulation, unit, cursorPosition);
+  GetFrequencyTypeInfo(band, modulation, unit, cursorPosition);
 
   lcd.setCursor(0, 0);
   lcd.print(modulation);
 
-  lcd.setCursor(5, 0);
-  if ((freq.length() == 4 && type == FM) || (freq.length() == 3 && type == AM))
+  lcd.setCursor(6, 0);
+  if ((freq.length() == 4 && band == FM) || (freq.length() == 3 && band == AM))
     lcd.print(" ");
   lcd.print(freq);
   
@@ -89,26 +101,35 @@ void PrintStationData() {
   lcd.print(unit);
 }
 
-void GetFrequencyTypeInfo(FrequencyType type, String& modulation, String& unit, int& cursorPosition) {
+void Tune() {
+  
+}
+
+void GetFrequencyTypeInfo(FrequencyBand type, String& modulation, String& unit, int& cursorPosition) {
   if (type == FM) {
     modulation = "FM";
     unit = "MHz";
-    cursorPosition = 11;
+    cursorPosition = 12;
   } else if (type == AM) {
     modulation = "AM";
     unit = "kHz";
-    cursorPosition = 10;
+    cursorPosition = 11;
   }
 }
 
-void pollRotaryEncoder() {
-  int currentClkState = digitalRead(PIN_CLK);
+bool IsDown(int pin) {
+  static bool lastPressed;
 
-  if (lastClkState == LOW && currentClkState == HIGH) {
-    if (digitalRead(PIN_DT) == HIGH) {
-      count--;
-    } else {
-      count++;
-    }
+  bool pressed = digitalRead(pin) == LOW;
+
+  bool isDown;
+  if (pressed && !lastPressed) {
+    isDown = true;
+  } else {
+    isDown = false;
   }
+
+  lastPressed = pressed;
+
+  return isDown;
 }
